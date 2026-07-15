362a26e1b9259e3609c855465781a7ee9150af80
techprathamit97 <techprathamit@gmail.com>
2026-07-15 19:23:55 +0530

    Add Facebook Pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,25 +10,25 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    {/* Microsoft Clarity */}
-    <Script
-      id="microsoft-clarity"
-      strategy="afterInteractive"
-    >
-      {`
+      {/* Microsoft Clarity */}
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+      >
+        {`
         (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "u2rxsd55s3");
       `}
-    </Script>
-    {/* Facebook Pixel */}
-<Script
-  id="facebook-pixel"
-  strategy="afterInteractive"
->
-  {`
+      </Script>
+      {/* Facebook Pixel */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -48,7 +48,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     fbq('init', '4625299514420327');
     fbq('track', 'PageView');
   `}
-</Script>
+      </Script>
       {/* GTM HEAD SCRIPT */}
       <Script id="gtm-head" strategy="beforeInteractive">
         {`
@@ -76,14 +76,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         ></iframe>
       </noscript>
       <noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: "none" }}
-    src="https://www.facebook.com/tr?id=4625299514420327&ev=PageView&noscript=1"
-    alt=""
-  />
-</noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=4625299514420327&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
     </>
   );
 }
